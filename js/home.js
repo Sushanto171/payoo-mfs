000d4d888f2f4fbcdffde34b3916d8309d3aeb07
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2024-09-19 13:00:56 +0600

    added update balance ui/dom

--- js/home.js
+++ js/home.js
@@ -6,4 +6,16 @@ addMoneyButton.addEventListener('click', function(event){
     console.log(inputAmount, password);
     document.getElementById('password').value = '';
     document.getElementById('input-amount').value='';
+
+    if(password=== '1234'){
+        // console.log('adding money to your account')
+        const accountBalance=document.getElementById('account-balance').innerText;
+        const accountBalanceNumber = parseInt(accountBalance);
+        const inputAmountNumber = Number(inputAmount);
+        const updateBalance = accountBalanceNumber + inputAmountNumber;
+        document.getElementById('account-balance').innerText =updateBalance;
+    }
+    else{
+        alert('Failed to add money! Please try again.')
+    }
 })
